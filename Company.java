5cf60ce628ee1ac71c0ce082dd2356ae949cd204
Polina Rybachuk <polia6v@gmail.com>
2024-04-07 10:21:14 +0200

    zad4

--- Company.java
+++ Company.java
@@ -5,20 +5,22 @@ import employees.Worker;
 public class Company{
     public static void main(String[] args) {
 
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("Kacper Molewski", 10000.0, 0);
         employees[1] = new Worker("Kamila Sczetela", 8000.0, "Animator");
         employees[2] = new Employee("Polina Rybachuk", 8000.0);
         employees[3] = new Employee("Konrad Cepryński", 10000.0);
         employees[4] = new Worker("Filip Szokalski", 12000.0, "Motion designer");
+        employees[5] = new Manager("Kamil Molenda", 10000.0, 0);
+        employees[6] = new Worker("Maciej Łapiński", 5000.0, "Junior Project Manager");
 
         /* Pozostałość po zadaniu 1 
         System.out.println("Pracownik 4:");
         System.out.println(employees[3].toString());
 
         employees[3].setSalary(12500.0); */
-
+       
         int workers = 0;
         for (Employee employee : employees){
             if (!(employee instanceof Manager)) {
